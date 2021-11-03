1c4acf061e563802a3142a7ff65b3a5b9c2911a7
LCANOASUS <livingstone23@gmail.com>
2021-11-04 00:31:22 +0100

    Register Database

---
+++ XamarinApi/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using XamarinApi.Model;
+
+namespace XamarinApi
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Author> Autores { get; set; }
+
+    }
+}
